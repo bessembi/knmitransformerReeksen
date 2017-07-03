e00562e1d8dc3e09b261f5ace238ee9318b932a1
MartinRoth <marsianus@gmail.com>
2017-07-03 16:09:28 +0200

    Updated rrupper files with script

---
+++ rrupper/Create_rrupper_ref_files.R
@@ -0,0 +1,40 @@
+# ------------------------------------------------------------------------------
+# Transform tg temperature
+# ------------------------------------------------------------------------------
+
+
+# Always use up to date package
+install.packages("KNMI/knmitransformer")
+
+library(purrr)
+library(knmitransformer)
+
+# Obtain the reference series used in the knmitransformer package
+refData <- KnmiRefFile("KNMI14____ref_rrcentr___19810101-20101231_v3.2.txt")  
+
+# Copy the file here for the sake of completeness
+file.copy(refData, "rrupper/KNMI14____ref_rrupper.txt", overwrite = TRUE)
+
+
+fn <- function(scenario, horizon) {
+  filename <- "rrupper/KNMI14"
+  if (horizon == 2030) {
+    filename <- paste0(filename, "____2030")
+  } else {
+    filename <- paste0(filename, "_", scenario, "_", horizon)
+  }
+  filename <- paste0(filename, "_rrupper.txt")
+  TransformPrecip(input = refData,
+                scenario = scenario, horizon = horizon,
+                subscenario = "upper",
+                ofile = filename)
+}
+
+combinations <- expand.grid(scenario = c("GL", "GH", "WL", "WH"),
+                            horizon = c(2050, 2085),
+                            stringsAsFactors = FALSE)
+combinations <- rbind(combinations, c("GL", 2030))
+
+
+pwalk(combinations, fn)
+
