38c5779822aefd5d2db129a4547543efd2f87d6e
MartinRoth <marsianus@gmail.com>
2017-07-03 15:47:15 +0200

    Update rrcentr with script

---
+++ rrcentr/Create_rrcentr_ref_files.R
@@ -0,0 +1,40 @@
+# ------------------------------------------------------------------------------
+# Transform tg temperature
+# ------------------------------------------------------------------------------
+
+
+# Always use up to date package
+install.packages("KNMI/knmitransformer")
+
+library(purrr)
+library(knmitransformer)
+
+# Obtain the reference series used in the knmitransformer package
+refData <- KnmiRefFile("KNMI14____ref_rrcentr___19810101-20101231_v3.2.txt")  
+
+# Copy the file here for the sake of completeness
+file.copy(refData, "rrcentr/KNMI14____ref_rrcentr.txt", overwrite = TRUE)
+
+
+fn <- function(scenario, horizon) {
+  filename <- "rrcentr/KNMI14"
+  if (horizon == 2030) {
+    filename <- paste0(filename, "____2030")
+  } else {
+    filename <- paste0(filename, "_", scenario, "_", horizon)
+  }
+  filename <- paste0(filename, "_rrcentr.txt")
+  TransformPrecip(input = refData,
+                scenario = scenario, horizon = horizon,
+                subscenario = "centr",
+                ofile = filename)
+}
+
+combinations <- expand.grid(scenario = c("GL", "GH", "WL", "WH"),
+                            horizon = c(2050, 2085),
+                            stringsAsFactors = FALSE)
+combinations <- rbind(combinations, c("GL", 2030))
+
+
+pwalk(combinations, fn)
+
