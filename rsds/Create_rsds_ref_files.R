4c45c5890c62bd6d54ae03f107733eead0244de6
MartinRoth <marsianus@gmail.com>
2017-07-03 15:17:21 +0200

    Updated rsds with script

---
+++ rsds/Create_rsds_ref_files.R
@@ -0,0 +1,41 @@
+# ------------------------------------------------------------------------------
+# Transform tg temperature
+# ------------------------------------------------------------------------------
+
+
+# Always use up to date package
+install.packages("KNMI/knmitransformer")
+
+library(purrr)
+library(knmitransformer)
+
+# Obtain the reference series used in the knmitransformer package
+refData <- KnmiRefFile("KNMI14____ref_tx___19810101-20101231_v3.2.txt")  
+
+# Copy the file here for the sake of completeness
+file.copy(refData, "tx/KNMI14____ref_tx.txt", overwrite = TRUE)
+
+
+regions <- MatchRegionsOnStationId(ReadInput("tn", refData)$header[1, -1])
+
+fn <- function(scenario, horizon) {
+  filename <- "tx/KNMI14"
+  if (horizon == 2030) {
+    filename <- paste0(filename, "____2030")
+  } else {
+    filename <- paste0(filename, "_", scenario, "_", horizon)
+  }
+  filename <- paste0(filename, "_tx.txt")
+  TransformTemp(input = refData, var = "tx",
+                scenario = scenario, horizon = horizon,
+                regions = regions, ofile = filename)
+}
+
+combinations <- expand.grid(scenario = c("GL", "GH", "WL", "WH"),
+                            horizon = c(2050, 2085),
+                            stringsAsFactors = FALSE)
+combinations <- rbind(combinations, c("GL", 2030))
+
+
+pwalk(combinations, fn)
+
