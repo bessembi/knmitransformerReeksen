cea9df0dd6ce10979c573d8d69d1634d77643193
MartinRoth <marsianus@gmail.com>
2017-07-03 15:04:13 +0200

    Updated tn files with script

---
+++ tn/Create_tn_ref_files.R
@@ -0,0 +1,41 @@
+# ------------------------------------------------------------------------------
+# Transform tg temperature
+# ------------------------------------------------------------------------------
+
+
+# Always use up to date package
+install.packages("KNMI/knmitransformer")
+
+library(purrr)
+library(knmitransformer)
+
+# Obtain the reference series used in the knmitransformer package
+refData <- KnmiRefFile("KNMI14____ref_tn___19810101-20101231_v3.2.txt")  
+
+# Copy the file here for the sake of completeness
+file.copy(refData, "tn/KNMI14____ref_tn.txt", overwrite = TRUE)
+
+
+regions <- MatchRegionsOnStationId(ReadInput("tn", refData)$header[1, -1])
+
+fn <- function(scenario, horizon) {
+  filename <- "tn/KNMI14"
+  if (horizon == 2030) {
+    filename <- paste0(filename, "____2030")
+  } else {
+    filename <- paste0(filename, "_", scenario, "_", horizon)
+  }
+  filename <- paste0(filename, "_tn.txt")
+  TransformTemp(input = refData, var = "tn",
+                scenario = scenario, horizon = horizon,
+                regions = regions, ofile = filename)
+}
+
+combinations <- expand.grid(scenario = c("GL", "GH", "WL", "WH"),
+                            horizon = c(2050, 2085),
+                            stringsAsFactors = FALSE)
+combinations <- rbind(combinations, c("GL", 2030))
+
+
+pwalk(combinations, fn)
+
